a2eff527827a0efb3b7fba42166c5146569ce5b1
Fabrizio <fabrizio.grosa@cern.ch>
2025-10-07 21:33:35 +0200

    Add script for ratio calculation (#12)

---
+++ Ratios/compute_ratio.py
@@ -0,0 +1,242 @@
+import ROOT
+import numpy as np
+import argparse
+import yaml
+
+
+def get_raw_yields(config, cent_bin):
+    """
+    Retrieve raw yield histograms from a ROOT file based on centrality ranges.
+
+    Parameters:
+    - config (dict): Configuration dictionary containing input file paths.
+    - cent_bin (int): centrality bin
+
+    Returns:
+    - tuple: A tuple containing two ROOT histograms:
+        - h_raw_yields_ds: Histogram for raw yields of ds.
+        - h_raw_yields_dplus: Histogram for raw yields of dplus.
+    """
+    with ROOT.TFile.Open(config['inputs']['files']['raw_yields']) as in_file_raw_yields:
+        h_raw_yields_ds = in_file_raw_yields.Get(
+            config['inputs']['histo_names']['raw_yields_ds'][cent_bin])
+        h_raw_yields_ds.SetDirectory(0)
+        h_raw_yields_dplus = in_file_raw_yields.Get(
+            config['inputs']['histo_names']['raw_yields_dp'][cent_bin])
+        h_raw_yields_dplus.SetDirectory(0)
+
+    return h_raw_yields_ds, h_raw_yields_dplus
+
+
+def get_efficiencies(config, cent_bin):
+    """
+    Retrieve efficiency histograms for prompt Ds and D+ from a ROOT file.
+
+    Parameters:
+    - config (dict): Configuration dictionary containing input file paths.
+    - cent_bin (int): centrality bin
+
+    Returns:
+    - tuple: A tuple containing two ROOT histograms:
+        - h_eff_prompt_ds: Efficiency histogram for prompt Ds.
+        - h_eff_prompt_dplus: Efficiency histogram for prompt D+.
+    """
+    with ROOT.TFile.Open(config['inputs']['files']['eff']) as eff_file:
+        h_eff_prompt_ds = eff_file.Get(
+            config['inputs']['histo_names']['eff_ds'][cent_bin])
+        h_eff_prompt_ds.SetDirectory(0)
+        h_eff_prompt_dplus = eff_file.Get(
+            config['inputs']['histo_names']['eff_dp'][cent_bin])
+        h_eff_prompt_dplus.SetDirectory(0)
+
+    return h_eff_prompt_ds, h_eff_prompt_dplus
+
+
+def get_prompt_fractions(config, cent_bin):
+    """
+    Retrieves the prompt fractions histograms for Ds and D+ from the specified ROOT files.
+
+    Args:
+    - config (dict): Configuration dictionary containing input file paths.
+    - cent_bin (int): centrality bin
+
+    Returns:
+    - tuple: A tuple containing two ROOT histograms:
+        - h_prompt_frac_ds: Prompt fraction for Ds.
+        - h_prompt_frac_dplus: Prompt fraction for D+.
+    """
+    if config['inputs']['files']['frac_ds'][cent_bin] is None or config['inputs']['files']['frac_dp'][cent_bin] is None:
+        return None, None
+
+    with ROOT.TFile.Open(config['inputs']['files']['frac_ds'][cent_bin]) as in_file_prompt_frac_ds:
+        h_prompt_frac_ds = in_file_prompt_frac_ds.Get("hRawFracPrompt")
+        h_prompt_frac_ds.SetDirectory(0)
+
+    with ROOT.TFile.Open(config['inputs']['files']['frac_dp'][cent_bin]) as in_file_prompt_frac_dplus:
+        h_prompt_frac_dplus = in_file_prompt_frac_dplus.Get("hRawFracPrompt")
+        h_prompt_frac_dplus.SetDirectory(0)
+
+    return h_prompt_frac_ds, h_prompt_frac_dplus
+
+
+def get_ratio_vs_pt(
+    config, h_raw_yields_ds, h_raw_yields_dplus, h_eff_prompt_ds,
+    h_eff_prompt_dplus, h_prompt_frac_ds, h_prompt_frac_dplus
+    ):
+    """
+    Calculate the ratio of corrected yields of D_s^+ to D^+.
+
+    Parameters:
+    - config (dict): Configuration dictionary.
+    - h_raw_yields_ds (ROOT.TH1): Histogram of raw yields for D_s^+.
+    - h_raw_yields_dplus (ROOT.TH1): Histogram of raw yields for D^+.
+    - h_eff_prompt_ds (ROOT.TH1): Histogram of prompt efficiency for D_s^+.
+    - h_eff_prompt_dplus (ROOT.TH1): Histogram of prompt efficiency for D^+.
+    - h_prompt_frac_ds (ROOT.TH1): Histogram of prompt fraction for D_s^+.
+    - h_prompt_frac_dplus (ROOT.TH1): Histogram of prompt fraction for D^+.
+
+    Returns:
+        ROOT.TH1: Histogram of the ratio of corrected yields of D_s^+ to D^+.
+    """
+    h_corrected_yields_ds = h_raw_yields_ds.Clone("h_corrected_yields_ds")
+    h_corrected_yields_ds.Divide(h_eff_prompt_ds)
+    h_corrected_yields_ds.Scale(1 / config['br']['ds_to_phipi_to_kkpi'])
+    if h_prompt_frac_ds is not None:
+        h_corrected_yields_ds.Multiply(h_prompt_frac_ds)
+    else:
+        print("WARNING: computing ratios without feeddown correction")
+
+    h_corrected_yields_dplus = h_raw_yields_dplus.Clone("h_corrected_yields_dplus")
+    h_corrected_yields_dplus.Divide(h_eff_prompt_dplus)
+    h_corrected_yields_dplus.Scale(1 / config['br']['dplus_to_phipi_to_kkpi'])
+    if h_prompt_frac_dplus is not None:
+        h_corrected_yields_dplus.Multiply(h_prompt_frac_dplus)
+    else:
+        print("WARNING: computing ratios without feeddown correction")
+
+    h_ratio = h_corrected_yields_ds.Clone("h_ratio")
+    h_ratio.Divide(h_corrected_yields_dplus)
+    h_ratio.SetTitle(";#it{p}_{T} (GeV/#it{c});D_{s}^{+}/D^{+} Ratio")
+
+    return h_ratio
+
+
+def get_ratios_vs_cent(h_ratios_vs_pt, cent_mins, cent_maxs):
+    """
+    Generate histograms of ratios versus centrality from input histograms of ratios versus pT.
+
+    Parameters:
+    - h_ratios_vs_pt (list of ROOT.TH1): List of histograms representing ratios
+        versus pT for different centrality bins.
+    - cent_mins (list of float): List of minimum centrality values for each bin.
+    - cent_maxs (list of float): List of maximum centrality values for each bin.
+
+    Returns:
+    - histos (list of ROOT.TH1D): List of histograms representing ratios versus centrality.
+    """
+    h_ratios_vs_pt = h_ratios_vs_pt.copy()
+    cent_tuple = list(zip(cent_mins, cent_maxs))
+    if (0, 100) in cent_tuple:
+        idx_zero_hundred = cent_tuple.index((0, 100))
+        cent_mins.pop(idx_zero_hundred)
+        cent_maxs.pop(idx_zero_hundred)
+        h_ratios_vs_pt.pop(idx_zero_hundred)
+    cent_edges = np.asarray(cent_mins + [cent_maxs[-1]], "d")
+    histos = []
+    for i in range(h_ratios_vs_pt[0].GetNbinsX()):
+        suffix = f"{h_ratios_vs_pt[0].GetBinLowEdge(i+1)*10:.0f}_{h_ratios_vs_pt[0].GetBinLowEdge(i+2)*10:.0f}"
+        histos.append(ROOT.TH1D(
+            f"h_ratio_cent_{suffix}",
+            ";Centrality percentile;D_{s}^{+}/D^{+} Ratio",
+            len(cent_edges)-1, cent_edges
+        ))
+        histos[-1].SetDirectory(0)
+        for i_cent in range(len(cent_mins)):
+            histos[-1].SetBinContent(i_cent+1, h_ratios_vs_pt[i_cent].GetBinContent(i+1))
+            histos[-1].SetBinError(i_cent+1, h_ratios_vs_pt[i_cent].GetBinError(i+1))
+
+    return histos
+
+
+def get_ratios_vs_dndeta(h_ratios_vs_cent, cent_mins, cent_maxs, mult_values, mult_unc_low, mult_unc_high):
+    cent_tuple = list(zip(cent_mins, cent_maxs))
+    if (0, 100) in cent_tuple:
+        idx_zero_hundred = cent_tuple.index((0, 100))
+        cent_mins.pop(idx_zero_hundred)
+        cent_maxs.pop(idx_zero_hundred)
+        mult_values.pop(idx_zero_hundred)
+        mult_unc_low.pop(idx_zero_hundred)
+        mult_unc_high.pop(idx_zero_hundred)
+        h_ratios_vs_cent.pop(idx_zero_hundred)
+    graphs = []
+    for histo in h_ratios_vs_cent:
+        y, y_err = [], []
+        for i in range(histo.GetNbinsX()):
+            y.append(histo.GetBinContent(i+1))
+            y_err.append(histo.GetBinError(i+1))
+        x = np.asarray(mult_values, "d")
+        y = np.asarray(y, "d")
+        y_err = np.asarray(y_err, "d")
+        x_err_low = np.asarray(mult_unc_low, "d")
+        x_err_high = np.asarray(mult_unc_high, "d")
+        suffix = histo.GetName().split("h_ratio_cent")[-1]
+        graphs.append(ROOT.TGraphAsymmErrors(len(x), x, y, x_err_low, x_err_high, y_err, y_err))
+        graphs[-1].SetName(f"g_ratio_dndeta{suffix}")
+    return graphs
+
+def evaluate_ratio(config_file_name):
+    """
+    Evaluate the Ds+/D+ ratio and save the histograms to a ROOT file.
+
+    Parameters:
+        config_file_name (str): Path to the configuration YAML file.
+    """
+    with open(config_file_name, 'r', encoding="utf8") as file:
+        config = yaml.safe_load(file)
+
+    h_ratios_vs_pt = []
+    cent_mins = config["centrality"]["mins"]
+    cent_maxs = config["centrality"]["maxs"]
+    with ROOT.TFile(config['output_file'], "recreate") as output_file:
+        for icent, (cent_min, cent_max) in enumerate(zip(cent_mins, cent_maxs)):
+            h_rawy_ds, h_rawy_dplus = get_raw_yields(config, icent)
+            h_eff_ds_prompt, h_eff_dplus_prompt = get_efficiencies(config, icent)
+            h_fprompt_ds, h_fprompt_dplus = get_prompt_fractions(config, icent)
+            h_ratios_vs_pt.append(get_ratio_vs_pt(
+                config, h_rawy_ds, h_rawy_dplus, h_eff_ds_prompt,
+                h_eff_dplus_prompt, h_fprompt_ds, h_fprompt_dplus
+            ))
+            if cent_min is not None and cent_max is not None:
+                output_file.mkdir(f"centrality_{cent_min}_{cent_max}")
+                output_file.cd(f"centrality_{cent_min}_{cent_max}")
+
+            h_rawy_ds.Write()
+            h_rawy_dplus.Write()
+            h_eff_ds_prompt.Write()
+            h_eff_dplus_prompt.Write()
+            if h_fprompt_ds is not None:
+                h_fprompt_ds.Write()
+            if h_fprompt_dplus is not None:
+                h_fprompt_dplus.Write()
+            h_ratios_vs_pt[-1].Write()
+
+        if cent_mins is not None and cent_maxs is not None:
+            h_ratios_vs_cent = get_ratios_vs_cent(h_ratios_vs_pt, cent_mins, cent_maxs)
+            output_file.cd()
+            for h in h_ratios_vs_cent:
+                h.Write()
+            mult_values = config["mult"]["values"]
+            mult_unc_low = config["mult"]["unc_low"]
+            mult_unc_high = config["mult"]["unc_high"]
+            g_ratios_vs_dndeta = get_ratios_vs_dndeta(h_ratios_vs_cent, cent_mins, cent_maxs,
+                                                      mult_values, mult_unc_low, mult_unc_high)
+            for g in g_ratios_vs_dndeta:
+                g.Write()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calculate Ds+/D+ ratio')
+    parser.add_argument('config_file', metavar='text', help='Path to the config file')
+    args = parser.parse_args()
+
+    evaluate_ratio(args.config_file)
